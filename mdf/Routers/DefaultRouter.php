ca15b9a6eb8e4fba001597bb0b5bbdd2f0a41cc5
Mina <mina.dodunekova@gmail.com>
2015-04-30 09:13:40 +0300

    Finish default router

--- mdf/Routers/DefaultRouter.php
+++ mdf/Routers/DefaultRouter.php
@@ -10,11 +10,15 @@ namespace MDF\Routers;
 
 
 class DefaultRouter {
+    private $_controller = null;
+    private $_method = null;
+    private $_params = array();
+
 
     public function parse() {
         //echo '<pre>'.print_r($_SERVER, true).'</pre>';
         //some information servers do not return the $_SERVER['REQUEST_URI'] that is why I use $_SERVER['PHP_SELF']
-        $uri = substr(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']), 1);
+        $uri = substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME']) + 1);
         //echo $uri;
 
         $controller = null;
@@ -24,17 +28,32 @@ class DefaultRouter {
         //var_dump($params);
 
         if($params[0]) {
-            $controller = $params[0];
+            $this->_controller = ucfirst($params[0]);
             if($params[1]) {
-                $method = $params[1];
+                $this->_method = $params[1];
+
+                // leave only params in the $params array
+                unset($params[0], $params[1]);
+                $this->_params = array_values($params);
+
             }
-        } else {
-            $controller = 'index';
-            $method = 'index';
         }
 
-        echo 'controller: '. $controller . '<br>' . 'method: ' . $method;
-
+        echo 'controller: '. $this->_controller . '<br>' . 'method: ' . $this->_method;
+        var_dump($this->_params);
     }
+
+    public function getController() {
+        return $this->_controller;
+    }
+
+    public function getMethod() {
+        return $this->_method;
+    }
+
+    public function getGet() {
+        return $this->_params;
+    }
+
 
 }
