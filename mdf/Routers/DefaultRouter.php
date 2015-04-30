e2a6a88bd0b2b7d361b61f26bf01e0dbe4cf6c4d
Mina <mina.dodunekova@gmail.com>
2015-04-30 08:45:44 +0300

    Add controller/method logic to router

--- mdf/Routers/DefaultRouter.php
+++ mdf/Routers/DefaultRouter.php
@@ -12,7 +12,29 @@ namespace MDF\Routers;
 class DefaultRouter {
 
     public function parse() {
-        echo 'ok';
+        //echo '<pre>'.print_r($_SERVER, true).'</pre>';
+        //some information servers do not return the $_SERVER['REQUEST_URI'] that is why I use $_SERVER['PHP_SELF']
+        $uri = substr(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']), 1);
+        //echo $uri;
+
+        $controller = null;
+        $method = null;
+
+        $params = explode('/', $uri);
+        //var_dump($params);
+
+        if($params[0]) {
+            $controller = $params[0];
+            if($params[1]) {
+                $method = $params[1];
+            }
+        } else {
+            $controller = 'index';
+            $method = 'index';
+        }
+
+        echo 'controller: '. $controller . '<br>' . 'method: ' . $method;
+
     }
 
 }
