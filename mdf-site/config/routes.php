98561093efa0e222c34d540e606dedf14931e1a1
Mina <mina.dodunekova@gmail.com>
2015-05-03 08:08:19 +0300

    Added logic one and the same controller to be invoked for different params in uri (e.g. admin/administration); Added logic for the use of packages(e.g. administration package) Removed the dependency btw the names of the controller and the params in the uri.

---
+++ mdf-site/config/routes.php
@@ -0,0 +1,6 @@
+<?php
+$cnf['admin']['namespace'] = 'Controllers\Admin';
+$cnf['administration']['namespace'] = 'Controllers\Admin';
+//Default situation -> URI '/'
+$cnf['*']['namespace'] = 'Controllers';
+return $cnf;
--- mdf/FrontController.php
+++ mdf/FrontController.php
@@ -5,11 +5,37 @@ namespace MDF;
 
 class FrontController {
     private static $_instance = null;
+    private $_ns = null;
+    private $_controller = null;
+    private $_method = null;
 
     // decides which router should be invoked
     public function dispatch() {
         $test = new \MDF\Routers\DefaultRouter();
-        echo  $test->getURI();
+        $uri = $test->getURI();
+        //echo $uri;
+        $routes = \MDF\App::getInstance()->getConfig()->routes;
+
+        if(is_array($routes) && count($routes) > 0) {
+            foreach($routes as $key => $value) {
+                if(strpos($uri, $key) === 0) {
+                    $this->_ns = $value['namespace'];
+                    break;
+                }
+            }
+        } else {
+            throw new \Exception('Cannot load Controller!');
+        }
+
+
+        if($this->_ns == null && $routes['*']['namespace']) {
+            $this->_ns = $routes['*']['namespace'];
+        } else if($this->_ns == null && !$routes['*']['namespace']) {
+            throw new \Exception('No default controller provided!');
+        }
+
+        echo $this->_ns;
+
     }
 
     public function getDefaultController() {
