a6e97c52becd3f95c92b58681fd8c7ad5d8bdde2
Mina <mina.dodunekova@gmail.com>
2015-05-03 17:45:12 +0300

    Start work on db module

---
+++ mdf-site/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$cnf['default']['connection_string'] = 'mysql:host=localhost;dbname=test';
+$cnf['default']['username'] = 'root';
+$cnf['default']['pass'] = 'root';
+$cnf['default']['pdo_options'][PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES 'UTF8'";
+$cnf['default']['pdo_options'][PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+
+return $cnf;
---
+++ mdf/DB/SimpleDB.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MDF\DB;
+
+
+class SimpleDB {
+
+}
