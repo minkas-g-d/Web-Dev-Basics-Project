9f5410ccfe4c405acbb3a4e0143dea5ef98b60ae
Mina <mina.dodunekova@gmail.com>
2015-05-08 17:40:40 +0300

    set the right timezone add htaccess

--- mdf-site/public/index.php
+++ mdf-site/public/index.php
@@ -3,15 +3,21 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 include '../../mdf/App.php';
 
+date_default_timezone_set('Europe/Sofia');
+
 $app = \MDF\App::getInstance();
 $app->setRouter('DefaultRouter');
 $app->run();
 
+//echo date_default_timezone_get();
+//echo '<br>';
+//echo date('m/d/Y h:i:s a', time());
+
 
 // Test session
-$s = $app->getSession();
-$s->count+=1;
-echo $s->count;
+//$s = $app->getSession();
+//$s->count+=1;
+//echo $s->count;
 
 // DB test
 //$db = new \MDF\DB\SimpleDB();
