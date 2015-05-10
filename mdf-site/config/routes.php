a1f2f2c893a44082c6bf64dd11c258965a49807d
Mina <mina.dodunekova@gmail.com>
2015-05-10 17:23:57 +0300

    clean some unnecessary routs

--- mdf-site/config/routes.php
+++ mdf-site/config/routes.php
@@ -1,16 +1,8 @@
 <?php
-$cnf['admin']['namespace'] = 'Controllers\Admin';
-$cnf['administration']['namespace'] = 'Controllers\Admin';
 //$cnf['user']['namespace'] = 'Controllers\User';
 //$cnf['user']['controllers']['index']['to'] = 'index';
 //$cnf['user']['controllers']['index']['methods']['add-post'] = 'addPost';
 //$cnf['user']['controllers']['index']['methods']['new'] = 'addNewPost';
-
-// Provide the possibility to detach the names of the controllers from the provided params in the uri
-$cnf['administration']['controllers']['index']['to'] = 'index';
-$cnf['administration']['controllers']['index']['methods']['user'] = 'profile';
-
-$cnf['administration']['controllers']['new']['to'] = 'create';
 
 //Default situation -> URI '/'
 $cnf['*']['namespace'] = 'Controllers';
@@ -21,12 +13,12 @@ $cnf['*']['controllers']['posts']['methods']['view'] = 'view';
 $cnf['*']['controllers']['user']['index']['to'] = 'index';
 $cnf['*']['controllers']['user']['methods']['add-post'] = 'renderAddPost';
 $cnf['*']['controllers']['user']['methods']['new-post'] = 'addPost';
-
+$cnf['*']['controllers']['user']['methods']['delete-post'] = 'deletePost';
 
 $cnf['*']['controllers']['user']['methods']['register'] = 'renderRegister';
 $cnf['*']['controllers']['user']['methods']['new'] = 'register';
 
 $cnf['*']['controllers']['user']['methods']['login'] = 'renderLogin';
-$cnf['*']['controllers']['user']['methods']['signin'] = 'renderLogin';
+$cnf['*']['controllers']['user']['methods']['signin'] = 'login';
 
 return $cnf;
